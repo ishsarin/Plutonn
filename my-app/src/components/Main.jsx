8997f7b07a38ca65ebc3faeb04aa0e59d7ab8923
ishsarin <ishsarin2000@gmail.com>
2023-08-11 02:45:17 +0530

    left side started

---
+++ my-app/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { LeftSide } from "./LeftSide";
+export const Main = () => {
+  return (
+    <main className="row">
+      <section className="col-4">
+        <LeftSide />
+      </section>
+    </main>
+  );
+};
